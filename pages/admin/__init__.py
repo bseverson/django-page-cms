a68b64138c675d63be7a16e3cc21d0fd76ebdfaf
bseverson <beau@comfychairconsulting.com>
2015-03-16 11:37:11 -0500

    Update __init__.py
    Fix brackets again.

--- pages/admin/__init__.py
+++ pages/admin/__init__.py
@@ -284,9 +284,9 @@ class PageAdmin(admin.ModelAdmin):
             if addlanguage:
                 from six.moves import urllib
                 try:
-                    splitted = list(urllib.parse.urlparse(response.url)
+                    splitted = list(urllib.parse.urlparse(response.url))
                 except:
-                    splitted = list(urllib.parse.urlparse(response['location'])
+                    splitted = list(urllib.parse.urlparse(response['location']))
                 query = urllib.parse.parse_qs(splitted[4])
                 query['language'] = language
                 splitted[4] = urllib.parse.urlencode(query)
